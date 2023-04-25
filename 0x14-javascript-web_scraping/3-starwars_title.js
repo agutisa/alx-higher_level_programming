6668fb891cf91870db7f79a87c2f6e646aac4a9e
agutisa <agutisandra@gmail.com>
2023-04-25 13:56:58 +0300

    java script

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/';
+
+request.get(url + process.argv[2], function (error, response, body) {
+  if (error) {
+    console.error('error:', error);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
+});
